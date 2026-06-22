c8a5c01982824addf0ef006418cc7a2ce202e22d
AlexNen <abrahamcarengmail.com>
2026-06-22 17:37:59 +0530

    Updated frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Login Page</h1>} />
-        <Route path="/admin" element={<h1>Admin Module</h1>} />
-        <Route path="/receptionist" element={<h1>Receptionist Module</h1>} />
-        <Route path="/doctor" element={<h1>Doctor Module</h1>} />
-        <Route path="/labtech" element={<h1>Labtech Module</h1>} />
-        <Route path="/pharmacist" element={<h1>Pharmacist Module</h1>} />
+        <Route path="/admin" element={<h1>AdminDashboard</h1>} />
+        <Route path="/receptionist" element={<h1>ReceptionistDashboard</h1>} />
+        <Route path="/doctor" element={<h1>DoctorDashboard</h1>} />
+        <Route path="/labtech" element={<h1>LabtechDashboard</h1>} />
+        <Route path="/pharmacist" element={<h1>PharmacistDashboard</h1>} />
       </Routes>
     </BrowserRouter>
   );
